a52d3c9a44f345de65485e8c92dd7ae58a39ab6f
admin <thecodersfamily@gmail.com>
2019-10-02 13:13:47 +0530

    Added GA.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactGA from 'react-ga';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <h1 className="App-header">
-        The Coders Family
-      </h1>
-    </div>
-  );
+ReactGA.initialize('UA-149266959-1');
+
+class App extends Component {
+  componentDidMount() {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-header">
+          The Coders Family
+        </h1>
+      </div>
+    );
+  }
 }
 
 export default App;
